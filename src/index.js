866ea43a4d87f141d54889acb0e40e60c7e63cd2
Michael Brink <michaelbrink3@gmail.com>
2022-11-04 23:37:50 -0700

    add express/api endpoint

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@
 Use Minimal payload for a group of recipients 
 Adapted from PUSH SDK (Gasless) docs: https://docs.push.org/developers/developer-guides/sending-notifications/using-epns-sdk-gasless
 */
+import express from "express";
 
 import * as PushAPI from "@pushprotocol/restapi";
 import * as ethers from "ethers";
 import * as env from "dotenv";
+
+const app = express();
+const port = 8080;
 
 env.config();
 
@@ -13,27 +17,28 @@ const PK = process.env.apiKey; // channel private key
 const Pkey = `0x${PK}`;
 const signer = new ethers.Wallet(Pkey);
 
-const sendNotification = async () => {
+const sendNotification = async (recipients, title, msg, img) => {
+  /*
+    recipients = array of address to send notificationt to
+    msg = leaderboard or score update message
+    img = leaderboard or score update image (use flaticon.com?)
+    */
   try {
     const apiResponse = await PushAPI.payloads.sendNotification({
       signer,
       type: 4, // subset
       identityType: 2, // Minimal payload
       notification: {
-        title: `[SDK-TEST] notification TITLE:`,
-        body: `[sdk-test] notification BODY`,
+        title: title,
+        body: msg,
       },
       payload: {
-        title: `[sdk-test] Notification`,
-        body: `22:24`,
+        title: title,
+        body: msg,
         cta: "",
-        img: "",
+        img: img,
       },
-      //recipients: ["eip155:5:0xe9c079525aCe13822A7845774F163f27eb5f21Da"],
-      recipients: [
-        "eip155:5:0xe9c079525aCe13822A7845774F163f27eb5f21Da",
-        "eip155:5:0x691C7c07A1B1698c56340d386d8cC7A823f6e2D8",
-      ], // recipients address
+      recipients: eval(recipients),
       channel: "eip155:5:0x9017804aE02877C32739A7703400326e9Ac9a04d", // your channel address
       env: "staging",
     });
@@ -44,4 +49,16 @@ const sendNotification = async () => {
   }
 };
 
-sendNotification();
+app.post("/api", (req, res) => {
+  //console.log(req["query"]["recipients"]); //test returning recipient array
+  sendNotification(
+    req["query"]["recipients"],
+    req.headers["title"],
+    req.headers["msg"],
+    req.headers["img"]
+  );
+});
+
+app.listen(port, () => {
+  console.log(`PUSH Notification app listening on port ${port}`);
+});
