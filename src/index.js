fc7a184bc012db0f4c28d393aeaff4e29a78c3e9
Michael Brink <michaelbrink3@gmail.com>
2022-11-04 22:50:00 -0700

    config PUSK SDK

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ Adapted from PUSH SDK (Gasless) docs: https://docs.push.org/developers/developer
 
 import * as PushAPI from "@pushprotocol/restapi";
 import * as ethers from "ethers";
+import * as env from "dotenv";
+
+env.config();
 
 const PK = process.env.apiKey; // channel private key
 const Pkey = `0x${PK}`;
@@ -15,7 +18,7 @@ const sendNotification = async () => {
     const apiResponse = await PushAPI.payloads.sendNotification({
       signer,
       type: 4, // subset
-      identityType: 0, // Minimal payload
+      identityType: 2, // Minimal payload
       notification: {
         title: `[SDK-TEST] notification TITLE:`,
         body: `[sdk-test] notification BODY`,
@@ -26,14 +29,16 @@ const sendNotification = async () => {
         cta: "",
         img: "",
       },
-      recipients: ["eip155:5:0xe9c079525aCe13822A7845774F163f27eb5f21Da"],
-      //recipients: ['eip155:5:0xCdBE6D076e05c5875D90fa35cc85694E1EAFBBd1', 'eip155:5:0x52f856A160733A860ae7DC98DC71061bE33A28b3'], // recipients address
+      //recipients: ["eip155:5:0xe9c079525aCe13822A7845774F163f27eb5f21Da"],
+      recipients: [
+        "eip155:5:0xe9c079525aCe13822A7845774F163f27eb5f21Da",
+        "eip155:5:0x691C7c07A1B1698c56340d386d8cC7A823f6e2D8",
+      ], // recipients address
       channel: "eip155:5:0x9017804aE02877C32739A7703400326e9Ac9a04d", // your channel address
       env: "staging",
     });
 
-    // apiResponse?.status === 204, if sent successfully!
-    console.log("API repsonse: ", apiResponse);
+    console.log("sent!");
   } catch (err) {
     console.error("Error: ", err);
   }
